112d139dd1139c7aaad3a04d6a2a470627c1fb19
Nikesh Sharma <eagernikesh@gmail.com>
2022-03-04 22:26:52 +0545

    practise

---
+++ starter/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+// Blocking/Synchronous way
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+const textOut = `This is what we know about avacado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written");
+
+// Non-blocking/Asyncronous Way
+fs.readFile("", () => {});
